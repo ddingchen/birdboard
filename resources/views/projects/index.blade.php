3a1214c8ea3a8219707e087475113f105d1b2f84
ddingchen <183488175@qq.com>
2019-02-01 15:14:15 +0800

    Go Go Gadget Tailwind.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,11 +5,14 @@
         <h1 class="mr-auto">Birdboard</h1>
         <a href="/projects/create">New Project</a>
     </div>
-    <ul>
-    @forelse ($projects as $project)
-        <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
-    @empty
-        <p>No projects exists.</p>
-    @endforelse
-    </ul>
+    <div class="flex">
+        @forelse ($projects as $project)
+            <div class="bg-white mr-4 rounded shadow w-1/3 p-5">
+                <h3 class="font-normal text-xl mb-5 py-5">{{ $project->title }}</h3>
+                <div class="text-grey">{{ str_limit($project->description, 200) }}</div>
+            </div>
+        @empty
+            <p>No projects exists.</p>
+        @endforelse
+    </div>
 @endsection
