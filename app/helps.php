93e5214e63758248378626fb6e5582a68af355a6
ddingchen <183488175@qq.com>
2019-04-18 16:01:34 +0800

    修复模板编译错误

--- app/helps.php
+++ app/helps.php
@@ -1,7 +1,8 @@
 <?php
 
-function gravatar_path($email) {
-    return "https://www.gravatar.com/avatar/{{ $email }}?" . http_build_query([
+function gravatar_path($email)
+{
+    return "https://www.gravatar.com/avatar/" . $email . "?" . http_build_query([
         's' => 50,
         'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg',
     ]);
